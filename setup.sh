1c3c7ada7e4229a25c7ce3730bd279266b7809db
Mikalai Zubok <mikalai.zubok@gmail.com>
2017-08-08 23:31:13 -0400

    minor improvments

--- setup.sh
+++ setup.sh
@@ -8,16 +8,15 @@ rm ~/.inputrc
 rm ~/.wgetrc
 rm ~/.zshrc
 
-ln -s ~/Projects/dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/Projects/dotfiles/git/gitignore ~/.gitignore
-
-ln -s ~/Projects/dotfiles/shell/curlrc ~/.curlrc
-ln -s ~/Projects/dotfiles/shell/inputrc ~/.inputrc
-ln -s ~/Projects/dotfiles/shell/wgetrc ~/.wgetrc
-ln -s ~/Projects/dotfiles/shell/zshrc ~/.zshrc
+ln -s "$(pwd)/git/gitconfig" ~/.gitconfig
+ln -s "$(pwd)/git/gitignore" ~/.gitignore
+ln -s "$(pwd)/shell/curlrc" ~/.curlrc
+ln -s "$(pwd)/shell/inputrc" ~/.inputrc
+ln -s "$(pwd)/shell/wgetrc" ~/.wgetrc
+ln -s "$(pwd)/shell/zshrc" ~/.zshrc
 
 # Install the Dracula Custom theme for iTerm
-open "${HOME}/Projects/dotfiles/iterm/themes/Dracula Custom.itermcolors"
+open "$(pwd)/iterm/themes/Dracula Custom.itermcolors"
 
 # Reload zsh settings
 source ~/.zshrc
