8d15fc9e5d3b22f49f8cfa139796a770aa858a5b
Mikalai Zubok <mikalai.zubok@clarabridge.com>
2017-08-27 14:33:42 -0400

    new aliases

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -13,7 +13,26 @@ alias psg="ps aux | grep "
 
 # Show human friendly numbers and colors
 alias df="df -h"
-alias du="du -h -d 2"
+alias du="du -h -d 1"
 
 # Zippin
 alias gz='tar -zcvf'
+
+# ls, the common ones I use a lot shortened for rapid fire usage
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
+
+alias h='history'
+
+alias zshrc='$EDITOR ~/.zshrc' # Quick access to the ~/.zshrc file
+alias reload!='. ~/.zshrc'
+
+alias grep='grep --color'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
