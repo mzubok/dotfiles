0d0a8bcd039f7d3d5222d0637fe3dc1ef3021ef6
Mikalai Zubok <mikalai.zubok@clarabridge.com>
2017-09-04 23:45:23 -0400

    aliases and functions added to setup script

--- setup.sh
+++ setup.sh
@@ -7,12 +7,18 @@ rm ~/.inputrc
 rm ~/.wgetrc
 rm ~/.zshrc
 
+rm ~/.aliases
+rm ~/.functions
+
 ln -s "$(pwd)/git/gitconfig" ~/.gitconfig
 ln -s "$(pwd)/git/gitignore" ~/.gitignore
 ln -s "$(pwd)/shell/curlrc" ~/.curlrc
 ln -s "$(pwd)/shell/inputrc" ~/.inputrc
 ln -s "$(pwd)/shell/wgetrc" ~/.wgetrc
 ln -s "$(pwd)/shell/zshrc" ~/.zshrc
+
+ln -s "$(pwd)/shell/aliases" ~/.aliases
+ln -s "$(pwd)/shell/functions" ~/.functions
 
 # Install the Dracula Custom theme for iTerm
 open "$(pwd)/iterm/themes/Dracula Custom.itermcolors"
