e5cbbe1986282dcec4c43049b9c3650cec465433
Mikalai Zubok <mikalai.zubok@clarabridge.com>
2017-09-09 00:32:30 -0400

    new casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-brew cask install docker
-brew cask install forklift
-brew cask install grammarly
-brew cask install intellij-idea
-brew cask install iterm2
-brew cask install java
-brew cask install paw
-brew cask install slack
-brew cask install sourcetree
-brew cask install sublime-text
-brew cask install trailer
+# Install packages
+apps=(
+    alfred
+    docker
+    forklift
+    google-drive
+    grammarly
+    intellij-idea
+    iterm2
+    java
+    paw
+    slack
+    sourcetree
+    sublime-text
+    trailer
+)
+
+brew cask install "${apps[@]}"
 
 brew cask cleanup
