87e16fbf08cbd007f811c5f72f1b61d5c3006a50
Mikalai Zubok <mikalai.zubok@clarabridge.com>
2017-09-08 23:57:44 -0400

    New aliases

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -58,3 +58,18 @@ alias flushdns="dscacheutil -flushcache"
 # Shortcuts
 alias x+="chmod +x"
 alias -- +x="chmod +x"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+# Show active network interfaces
+alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
