5d2a76f0dc0821d314da353bb5f2b2b7e7d045e1
Mikalai Zubok <mikalai.zubok@gmail.com>
2019-09-15 18:13:42 +0300

    Replace ls with exa

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -21,11 +21,11 @@ alias du="du -h -d 1"
 alias gz='tar -zcvf'
 
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
+alias l='exa -lFh'    # size,show type,human readable
+alias la='exa -alFhg' # long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   # sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   # long list,sorted by date,show type,human readable
+alias ll='ls -l'      # long list
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
