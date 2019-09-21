c78790a3390df030050bb9744097e2ff73dccc54
Mikalai Zubok <mikalai.zubok@gmail.com>
2019-09-21 21:57:05 +0300

    Adding ssh completion

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -76,3 +76,6 @@ alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup
 
 alias ping='prettyping --nolegend'
 alias du="ncdu --color dark -rr -x --exclude .git"
+
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
