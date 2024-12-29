79572caf752cd2c81fdaab1b9f7b9a6aee72e7fb
Mikalai Zubok <mikalai.zubok@gmail.com>
2024-12-29 11:59:14 +0200

    Move zsh config to dedicate folder

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ rm ~/.curlrc
 rm ~/.inputrc
 rm ~/.screenrc
 rm ~/.wgetrc
+rm ~/.zprofile
 rm ~/.zshrc
 
 echo 'Creating new symbolic links...'
@@ -26,18 +27,13 @@ ln -s "$(pwd)/shell/curlrc" ~/.curlrc
 ln -s "$(pwd)/shell/inputrc" ~/.inputrc
 ln -s "$(pwd)/shell/screenrc" ~/.screenrc
 ln -s "$(pwd)/shell/wgetrc" ~/.wgetrc
-ln -s "$(pwd)/shell/zshrc" ~/.zshrc
+ln -s "$(pwd)/zsh/zprofile" ~/.zprofile
+ln -s "$(pwd)/zsh/zshrc" ~/.zshrc
 
 mkdir ~/.bin
 ln -s "$(pwd)/shell/aliases.zsh" ~/.bin/aliases.zsh
 ln -s "$(pwd)/shell/config.zsh" ~/.bin/config.zsh
 ln -s "$(pwd)/shell/functions.zsh" ~/.bin/functions.zsh
-
-
-if [ platform == 'Darwin' ]; then
-    echo 'Instlling iTerm2 theme...'
-    open "$(pwd)/iterm/themes/Dracula Custom.itermcolors"
-fi
 
 # Reload zsh settings
 # source ~/.zshrc
