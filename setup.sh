44d9cc6c3669988c61ebe21adf7c388bc57d1014
Mikalai Zubok <mikalai_zubok@epam.com>
2025-11-13 08:51:43 +0200

    Fixing setup

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ FILES_TO_REMOVE=(
 
 CONFIG_FILES_TO_REMOVE=(
     ~/.config/atuin
+    ~/.config/bat
+    ~/.config/btop
     ~/.config/ghostty
     ~/.config/starship.toml
     ~/.config/zed/settings.json
@@ -52,34 +54,40 @@ echo 'Creating new symbolic links...'
 # Get the absolute path of the current directory
 BASE_DIR="$(pwd)"
 
-declare -A SYMLINKS=(
-    ["$BASE_DIR/ack/ackrc"]="~/.ackrc"
-    ["$BASE_DIR/git/gitconfig"]="~/.gitconfig"
-    ["$BASE_DIR/git/gitignore"]="~/.gitignore"
-    ["$BASE_DIR/shell/editorconfig"]="~/.editorconfig"
-    ["$BASE_DIR/shell/curlrc"]="~/.curlrc"
-    ["$BASE_DIR/shell/inputrc"]="~/.inputrc"
-    ["$BASE_DIR/ripgrep/ripgreprc"]="~/.ripgreprc"
-    ["$BASE_DIR/shell/screenrc"]="~/.screenrc"
-    ["$BASE_DIR/shell/wgetrc"]="~/.wgetrc"
-    ["$BASE_DIR/zsh/zprofile"]="~/.zprofile"
-    ["$BASE_DIR/zsh/zshrc"]="~/.zshrc"
-    ["$BASE_DIR/atuin"]="~/.config/atuin"
-    ["$BASE_DIR/ghostty"]="~/.config/ghostty"
-    ["$BASE_DIR/starship/starship.toml"]="~/.config/starship.toml"
-    ["$BASE_DIR/zed/settings.json"]="~/.config/zed/settings.json"
-    ["$BASE_DIR/shell/aliases.zsh"]="~/.bin/aliases.zsh"
-    ["$BASE_DIR/shell/config.zsh"]="~/.bin/config.zsh"
-    ["$BASE_DIR/shell/functions.zsh"]="~/.bin/functions.zsh"
-)
-
-# Create symlinks
-for src in "${!SYMLINKS[@]}"; do
-    dest="${SYMLINKS[$src]}"
+# Create symlinks using a simpler approach
+create_symlink() {
+    local src="$1"
+    local dest="$2"
+    
+    # Expand tilde in destination
+    dest="${dest/#\~/$HOME}"
+    
     if [ -L "$dest" ] || [ -e "$dest" ]; then
         rm -f "$dest"
     fi
-    ln -s "$(readlink -f "$src")" "$dest"
-done
+    ln -s "$src" "$dest"
+}
+
+# Create all symlinks
+create_symlink "$BASE_DIR/ack/ackrc" ~/.ackrc
+create_symlink "$BASE_DIR/git/gitconfig" ~/.gitconfig
+create_symlink "$BASE_DIR/git/gitignore" ~/.gitignore
+create_symlink "$BASE_DIR/shell/editorconfig" ~/.editorconfig
+create_symlink "$BASE_DIR/shell/curlrc" ~/.curlrc
+create_symlink "$BASE_DIR/shell/inputrc" ~/.inputrc
+create_symlink "$BASE_DIR/ripgrep/ripgreprc" ~/.ripgreprc
+create_symlink "$BASE_DIR/shell/screenrc" ~/.screenrc
+create_symlink "$BASE_DIR/shell/wgetrc" ~/.wgetrc
+create_symlink "$BASE_DIR/zsh/zprofile" ~/.zprofile
+create_symlink "$BASE_DIR/zsh/zshrc" ~/.zshrc
+create_symlink "$BASE_DIR/atuin" ~/.config/atuin
+create_symlink "$BASE_DIR/bat" ~/.config/bat
+create_symlink "$BASE_DIR/btop" ~/.config/btop
+create_symlink "$BASE_DIR/ghostty" ~/.config/ghostty
+create_symlink "$BASE_DIR/starship/starship.toml" ~/.config/starship.toml
+create_symlink "$BASE_DIR/zed/settings.json" ~/.config/zed/settings.json
+create_symlink "$BASE_DIR/shell/aliases.zsh" ~/.bin/aliases.zsh
+create_symlink "$BASE_DIR/shell/config.zsh" ~/.bin/config.zsh
+create_symlink "$BASE_DIR/shell/functions.zsh" ~/.bin/functions.zsh
 
 echo "Setup is done. Run 'reload!' now!"
